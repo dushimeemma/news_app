3d4b56b19dc20183ebac78498d1efe01b8530e9d
dushimeemma <dushimeemma@gmail.com>
2024-01-25 17:10:13 +0200

    flutter clean architecture

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,6 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = '5cd374627218403281feb4dff6333cb7';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
+const String imageNotFound =
+    'https://firebasestorage.googleapis.com/v0/b/hir-mobile-app.appspot.com/o/image-not-found.png?alt=media&token=3d0bf9c9-deb6-4386-9239-58dd07450c1f';
