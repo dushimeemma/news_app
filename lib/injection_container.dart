bdce9de28a322375711cd450b084f52a4647e5c7
dushimeemma <dushimeemma@gmail.com>
2024-01-26 13:00:05 +0200

    display saved articles

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -5,6 +5,10 @@ import 'package:news_app/features/daily_news/data/data_sources/remote/news_api_s
 import 'package:news_app/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:news_app/features/daily_news/domain/repository/article_repository.dart';
 import 'package:news_app/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app/features/daily_news/domain/usecases/get_saved_article.dart';
+import 'package:news_app/features/daily_news/domain/usecases/remove_article.dart';
+import 'package:news_app/features/daily_news/domain/usecases/save_article.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart';
 import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 
 final sl = GetIt.instance;
@@ -15,7 +19,12 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<AppDatabase>(database);
   sl.registerSingleton<Dio>(Dio());
   sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
-  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl()));
+  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl(), sl()));
   sl.registerSingleton<GetArticleUseCase>(GetArticleUseCase(sl()));
+  sl.registerSingleton<GetSavedArticleUseCase>(GetSavedArticleUseCase(sl()));
+  sl.registerSingleton<SaveArticleUseCase>(SaveArticleUseCase(sl()));
+  sl.registerSingleton<RemoveArticleUseCase>(RemoveArticleUseCase(sl()));
   sl.registerFactory<RemoteArticlesBloc>(() => RemoteArticlesBloc(sl()));
+  sl.registerFactory<LocalArticlesBloc>(
+      () => LocalArticlesBloc(sl(), sl(), sl()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news_app/config/theme/app_themes.dart';
-import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app/config/routes/routes.dart';
 import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
 import 'package:news_app/features/daily_news/presentation/pages/home/daily_news.dart';
-import 'package:news_app/injection_container.dart';
+import 'config/theme/app_themes.dart';
+import 'features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'injection_container.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider<RemoteArticlesBloc>(
       create: (context) => sl()..add(const GetArticles()),
       child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: const DailyNews(),
-        theme: theme(),
-      ),
+          debugShowCheckedModeBanner: false,
+          theme: theme(),
+          onGenerateRoute: AppRoutes.onGenerateRoutes,
+          home: const DailyNews()),
     );
   }
 }
